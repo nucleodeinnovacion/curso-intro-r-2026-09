09a7eb6bc3cc82583de2e907690fd59d07130df7
Joaquin Lovizio Ramos <joaquin.lovizio@gmail.com>
2026-09-01 22:00:58 -0300

    update

---
+++ scripts/clase1.R
@@ -0,0 +1,19 @@
+# CLASE 1 ####
+
+## 1 Revisamos entorno de trabajo ####
+getwd()
+setwd()
+
+## 2 - Creamos una carpeta dentro del entorno de traabajo ####
+
+# chequeamos si existe la carpeta
+dir.exists("./una_carpeta")
+
+# como no existe, la creamos
+dir.create("./una_carpeta")
+
+# volvemos a chequear
+dir.exists("./una_carpeta")
+
+
+
